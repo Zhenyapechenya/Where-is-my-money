a22e570d6d09ad286dcd32448ab81e085c2ccf5d
Zhenyapechenya <epsareva77@gmail.com>
2023-04-11 18:00:48 +0700

    trying validation

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+from enum import Enum
+
 
 app = FastAPI(
     title="Traiding App"
 )
 
 
+
 fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 1, "role": "admin", "name": ["Bob"]},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
 ]
 
 fake_trades = [
@@ -16,31 +25,61 @@ fake_trades = [
     {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
 ]
 
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
+plants = [
+    {"id": 1, "type": "leaf", "name": "Billy"},
+    {"id": 2, "type": "bloom", "name": "Antony"},
+    {"id": 3, "type": "cactus", "name": "Cassy"},
 ]
 
 
 
-@app.get("/users/{user_id}")
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = [] # указала, что параметр необязательный, звание может отсутствовать
+
+
+
+
+@app.get("/users/{user_id}", response_model=List[User]) # дополнительно указываю модель данных, которыми отвечает ф-я
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
-
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):#limit ограничивает количество получаемых сделок, 
-                                        # offset - размер сдвига для пагинации
-                                        # 1 - значения по умолчанию
-    return fake_trades[offset:][:limit]
+@app.get("/plants/{plant_id}")
+def get_user(plant_id: int):
+    return [plant for plant in plants if plant.get("id") == plant_id]
 
 
-@app.post("users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
+
+
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=6) # установила ограничение: не длиннее 5 символов
+    side: str
+    price: float = Field(ge=0) # установила ограничение: больше, чем 0
+    amount: float
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
+    
 
     
 
